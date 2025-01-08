827b626b2d5cea205f82fa0f80b9dad049e6ca2c
electrototo <cristobal@liendo.net>
2025-01-08 07:00:43 -0800

    Switch to mvc

--- src/controllers/link.ts
+++ src/controllers/link.ts
@@ -1,16 +1,65 @@
-// import { Link } from "src/models/link";
-// 
-// export class LinkController {
-//     create(url: String): Link {
-//         // Will either create a short link, or return 
-//     }
-// 
-//     delete(): void {
-//     }
-// 
-//     getLinkFromShortLink(): Link {
-//     }
-// 
-//     getShortLinkFromLink(): Link {
-//     }
-// }
+import { DDBLink, Link } from "../models/link";
+
+export interface LinkControllerProps {
+}
+
+export class LinkController {
+    alphabet: string = 'abcdefghijklmonpqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    baseDomain = 'https://s.to';
+
+    constructor(props: LinkControllerProps) {
+    }
+
+    async retrieve(props: { shortcode?: string, url?: string }): Promise<Link | undefined> {
+        const { url, shortcode } = props;
+
+        if (url === undefined && shortcode === undefined || url !== undefined && shortcode !== undefined) {
+            throw new Error('Either url or short code need to be defined');
+        }
+
+        const response = url !== undefined ?
+            await DDBLink.query.byUrl({ url }).go() :
+            await DDBLink.query.byShortCode({ shortcode: shortcode! }).go();
+
+        if (response.data.length === 0) {
+            return undefined;
+        }
+
+        const entry = response.data[0]!;
+
+        return {
+            url: entry.url,
+            shortcode: entry.shortcode
+        };
+    }
+
+    async create(url: string): Promise<Link> {
+        // Will either shorten the link or return the already shortened version of the link
+        const link: Link | undefined = await this.retrieve({ url });
+
+        if (link) {
+            return link;
+        }
+
+        const shortCode = this.createShortCode();
+        const response = await DDBLink.create({
+            url: url,
+            shortcode: `${this.baseDomain}/${shortCode}`
+        }).go();
+
+        return {
+            url: response.data.url,
+            shortcode: response.data.shortcode
+        }
+    }
+
+    createShortCode(length: number = 7): string {
+        let shortCode: string = '';
+
+        for (let i = 0; i < 7; i++) {
+            shortCode = shortCode + this.alphabet[Math.floor(Math.random() * this.alphabet.length)];
+        }
+
+        return shortCode;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,42 @@
 import cors from 'cors';
-import express, { Express, Request } from 'express';
-import { DDBLink } from './models/link';
+import express, { Express } from 'express';
+import { DDBLink, Link } from './models/link';
+import { LinkController } from './controllers/link';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const alphabet = 'abcdefghijklmonpqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
 const baseDomain = 'https://s.to';
 
 app.use(cors());
 app.use(express.json());
 
+const linkController = new LinkController({});
+
 app.get('/', async (req, res) => {
     const links = await DDBLink.scan.go();
 
-    console.log(links);
     res.send(JSON.stringify(links));
 });
 
 app.post('/', async (req, res) => {
     // try and find if the URL has not been created before
     const url = req.body.url;
-    const response = await DDBLink.query.byUrl({ url }).go();
+    const link = await linkController.create(url);
 
-    if (response.data.length > 0) {
-        const shortened = response.data[0];
-
-        res.send(JSON.stringify({
-            url,
-            shortCode: shortened?.shortcode
-        }));
-
-        return;
-    }
-
-    let shortCode: string = '';
-    for (let i = 0; i < 7; i++) {
-        shortCode = shortCode + alphabet[Math.floor(Math.random() * alphabet.length)];
-    }
-
-    const shortened = `${baseDomain}/${shortCode}`;
-
-    await DDBLink.create({
-        url: req.body.url,
-        shortcode: shortened
-    }).go();
-
-    res.send(JSON.stringify({
-        url: req.body.url,
-        shortcode: shortened
-    }));
+    res.send(JSON.stringify(link));
 });
 
 app.get('/:shortCode', async (req, res) => {
     const shortCode = `${baseDomain}/${req.params.shortCode}`;
-    const response = await DDBLink.get({
-        shortcode: shortCode
-    }).go();
+    const link: Link | undefined = await linkController.retrieve({ shortcode: shortCode });
 
-    console.log(`shortCode: ${req.params.shortCode}, response`, response);
-
-    if (!response.data?.url) {
+    if (!link) {
         res.status(404).end();
         return;
     }
 
-    res.redirect(response.data!.url);
+    res.redirect(link.url);
 });
 
 app.listen(port, () => {
